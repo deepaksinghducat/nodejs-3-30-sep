cbe2bc360575fcc2c9d19ca171c3f81526038b41
deepaksinghducat <deepaksinghducat@gmail.com>
2022-09-03 16:53:44 +0530

    first server

---
+++ nodejs-basic/server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const server = http.createServer((req,res) => {
+	console.log(req.method);
+});
+
+server.listen(3000,() => {
+	console.log('server is listening on port 3000');
+},)
