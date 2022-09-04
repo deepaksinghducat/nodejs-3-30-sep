19c23c3e9ecf9da1be48a54ac921c9bee26f160b
deepaksinghducat <deepaksinghducat@gmail.com>
2022-09-04 16:35:47 +0530

    upto request

--- nodejs-basic/server.js
+++ nodejs-basic/server.js
@@ -1,7 +1,33 @@
 const http = require('http');
 
 const server = http.createServer((req,res) => {
-	console.log(req.method);
+
+	
+	if(req.url === '/post' && req.method === 'POST') { 
+		console.log('fdsafsadf');
+
+		res.write('fdsafsadf');
+
+		res.end();
+	}
+
+	if(req.url === '/') {
+		res.write(`
+		<html>
+		    <head>
+		       <title>First App</title>
+		    </head>
+		    <body>
+		        <form action='/post' method='POST'>
+					<input type='text' name='name' />
+					<input type='submit' value='submit' />
+				</form>
+		    </body>
+		</html>`)
+
+		res.end();
+	}
+
 });
 
 server.listen(3000,() => {
