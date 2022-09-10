8f6f80f4b40896f8103294119ae3a58bcfd22b0c
deepaksinghducat <deepaksinghducat@gmail.com>
2022-09-10 16:32:32 +0530

    upto express start

--- nodejs-basic/server.js
+++ nodejs-basic/server.js
@@ -1,35 +1,75 @@
 const http = require('http');
 
-const server = http.createServer((req,res) => {
+const express = require('express');
+var bodyParser = require('body-parser')
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
+
+
+// const server = http.createServer((req,res) => {
 
 	
-	if(req.url === '/post' && req.method === 'POST') { 
-		console.log('fdsafsadf');
+// 	if(req.url === '/post' && req.method === 'POST') { 
 
-		res.write('fdsafsadf');
+// 		res.write('fdsafsadf');
 
-		res.end();
+// 		const body = []
+
+// 		req.on('data', function (data) {
+// 			body.push(data)
+// 		})
+
+// 		req.on('end', function () {
+// 		    console.log(body.toString());
+// 		})
+
+// 		res.end();
+// 	}
+
+// 	if(req.url === '/') {
+// 		res.write(`
+// 		<html>
+// 		    <head>
+// 		       <title>First App</title>
+// 		    </head>
+// 		    <body>
+// 		        <form action='/post' method='POST'>
+// 					<input type='text' name='name' />
+// 					<input type='submit' value='submit' />
+// 				</form>
+// 		    </body>
+// 		</html>`)
+
+// 		res.end();
+// 	}
+
+// });
+
+// server.listen(3000,() => {
+// 	console.log('server is listening on port 3000');
+// },)
+
+const isAuthenticated = 1;
+
+app.use('/', (req,res,next) => {
+    if(isAuthenticated) {
+		next();
+	}else{
+		res.send('not authenticated');
 	}
+})
 
-	if(req.url === '/') {
-		res.write(`
-		<html>
-		    <head>
-		       <title>First App</title>
-		    </head>
-		    <body>
-		        <form action='/post' method='POST'>
-					<input type='text' name='name' />
-					<input type='submit' value='submit' />
-				</form>
-		    </body>
-		</html>`)
-
-		res.end();
-	}
-
+app.post('/send-data', (req, res, next) => {
+	res.send(req.body)
 });
 
-server.listen(3000,() => {
+app.get('/fdsafadsfdsaf', (req,res) => {
+	res.send(req.method);
+})
+
+app.listen(3000,() => {
 	console.log('server is listening on port 3000');
-},)
+})
